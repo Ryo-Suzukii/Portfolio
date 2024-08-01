0ff8dd2679adb4d3c3a0b6d889ea6b4110f46fab
Ryo Suzuki <astronautryosuzuki@gmail.com>
2024-08-01 22:32:28 +0900

    Add : map

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,14 @@ import Top from '../components/AppTop.vue';
 import Profile from '../components/AppProfile.vue';
 import Works from '../components/AppWorks.vue';
 import Contact from '../components/AppContact.vue';
+import Map from '../components/AppMap.vue';
 
 const routes = [
   { path: '/', component: Top },
   { path: '/profile', component: Profile },
   { path: '/works', component: Works },
   { path: '/contact', component: Contact },
+  { path: '/map', component: Map },
 ];
 
 const router = createRouter({
